19e8737dcecff381a8ffccac8f194ccf0667ba09
WAJAHAT-ANSARI2 <m.wajahatansari2004@gmail.com>
2025-01-15 10:24:35 +0000

    home

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Components/Header";
+
+const App = () => {
+  return(
+    <div className="w-full overflow-hidden ">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
